cbeb4766ddd44c6cb40daa67c4b02174282106ae
9BH -j <Jehad.Shaleh@gmail.com>
2024-12-02 20:59:35 +0300

    lab 8

--- Project/apps/bookmodule/urls.py
+++ Project/apps/bookmodule/urls.py
@@ -13,5 +13,11 @@ urlpatterns = [
     path('html5/tables',views.viewTable, name="books.tables"),
     path('search/',views.viewSearch,name='books.search'),
     path('simple/query',views.simple_query,name='books.simple_query'),
-    path('complex/query',views.complex_query,name='books.complex_query')
+    path('complex/query',views.complex_query,name='books.complex_query'),
+    path('lab8/task1', views.book_list_task1, name='book_list_task1'),
+    path('lab8/task2', views.book_list_task2, name='book_list_task2'),
+    path('lab8/task3', views.book_list_task3, name='book_list_task3'),
+    path('lab8/task4', views.book_list_task4, name='book_list_task4'),
+    path('lab8/task5', views.book_list_task5, name='book_list_task5'),
+    path('students/lab8/task7', views.students_per_city, name='students_per_city')
 ]
